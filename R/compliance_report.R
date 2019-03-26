ff545b58ec6c4dff4723145784153ab26e9e77a7
Nelson Roque <nelsonroquejr@gmail.com>
2019-03-26 13:33:40 -0400

    up

--- R/compliance_report.R
+++ R/compliance_report.R
@@ -2,18 +2,18 @@
 
 #' @name compliance_report
 #' @param df class: numeric; original X
-#' @param id_var class: string
-#' @param session_var class: numeric; moved to X
+#' @param ... class string, vector
 #' @keywords m2c2, cognition
 #' @import tidyverse
 #' @examples
 #' compliance_report(df)
 #' @export
 
-compliance_report <- function(df, id_var = "ID", session_var = "session", id_cols = c(1:20)) {
-  report.df <- df %>% 
-    group_by_(id_var) %>%
-    summarise(n.records = n(),
-              n.unique.sessions = length(unique(session)))
+compliance_report <- function(df, ...) {
+  group_var <- enquos(...)
+  
+  report.df <- df %>%
+    group_by(!!! group_var) %>%
+    summarise(n.records = n())
   return(report.df)
 }
