7bcb47230da8762b2e2381ba7ad56fc6e62715d8
Nelson Roque <nelsonroquejr@gmail.com>
2019-03-26 13:08:37 -0400

    up

--- R/compliance_report.R
+++ R/compliance_report.R
@@ -1,3 +1,15 @@
+#' RM2C2: Scoring, Summarizing
+
+#' @name compliance_report
+#' @param df class: numeric; original X
+#' @param id_var class: string
+#' @param session_var class: numeric; moved to X
+#' @keywords m2c2, cognition
+#' @import tidyverse
+#' @examples
+#' compliance_report(df)
+#' @export
+
 compliance_report <- function(df, id_var = "ID", session_var = "session", id_cols = c(1:20)) {
   report.df <- df %>% 
     group_by_(id_var) %>%
