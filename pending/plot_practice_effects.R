b6aa7da59ea256bc303bb3cc5c814d42b748ea9e
Nelson Roque <nelsonroquejr@gmail.com>
2019-04-11 15:32:41 -0400

    up

--- pending/plot_practice_effects.R
+++ pending/plot_practice_effects.R
@@ -2,31 +2,38 @@ library(tidyverse)
 library(RM2C2)
 
 
-plot_practice_effect <- function(df = NA, filename = NA, y_var = NA, time_var = NA) {
+plot_practice_effect <- function(df = NA, filename = NA, y_var = NA, id_var = NA, time_var = NA) {
 
+  # get unique ids
+  qid <- quo(id_var)
+  unique.ids <- df %>% select(!! qid) %>% distinct(!! qid) %>% distinct()
+  
+  # open PDF
   pdf(file=filename)  
   
-  unique.ids <- eval(expr(unique(df$idc)))
-  
   for(i in unique.ids) {
+    # get summary of person series
+    summary_method <- paste0("mean(",id_var, ",na.rm=T)")
     
     # subset data
     temp.df <- df %>% 
-      filter(idc == i) %>%
-      group_by(idc, studyday) %>%
-      summarise(m = mean(cs_pr, na.rm=T))
+      filter(!! qid == i) %>%
+      group_by_at(c(id_var, time_var)) %>%
+      summarise_(.dots = set_names(summary_method, "value"))
     
     # draw plot
-    p <- ggplot(temp.df, aes(studyday, m)) + 
+    p <- ggplot(temp.df, aes(time_var, value)) + 
       geom_point() +
       geom_smooth(method="lm") +
-      ylim(-1,1) +
-      xlim(-5,30) +
+      #ylim(-1,1) +
+      #xlim(-5,30) +
       ggtitle(paste0("Participant ID: ",i))
     
+    # display plot in PDF
     print(p)
     
     # for debugging
+    print(summary_method)
     print(i)
     print(nrow(temp.df))
     print("-----")
@@ -35,6 +42,8 @@ plot_practice_effect <- function(df = NA, filename = NA, y_var = NA, time_var = 
     rm(temp.df)
   }
   
-  dev.off() 
+  #dev.off() 
   
 }
+
+plot_practice_effect(df, filename="test.pdf", y_var="spat_dmn", id_var="id", time_var="studyday")
