aa67b0197e7fc1f7115c46f6db4a12bbcba9322a
Nelson Roque <nelsonroquejr@gmail.com>
2019-04-11 14:15:10 -0400

    up

---
+++ pending/plot_practice_effects.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(RM2C2)
+
+
+plot_practice_effect <- function(df = NA, filename = NA, y_var = NA, time_var = NA) {
+
+  pdf(file=filename)  
+  
+  unique.ids <- eval(expr(unique(df$idc)))
+  
+  for(i in unique.ids) {
+    
+    # subset data
+    temp.df <- df %>% 
+      filter(idc == i) %>%
+      group_by(idc, studyday) %>%
+      summarise(m = mean(cs_pr, na.rm=T))
+    
+    p <- ggplot(temp.df, aes(studyday, m)) + 
+      geom_point() +
+      geom_smooth(method="lm") +
+      ylim(-1,1) +
+      xlim(-5,30) +
+      ggtitle(paste0("Participant ID: ",i))
+    
+    print(p)
+    
+    # for debugging
+    print(i)
+    print(nrow(temp.df))
+    print("-----")
+    
+    # remove old data
+    rm(temp.df)
+  }
+  
+  dev.off() 
+  
+}
