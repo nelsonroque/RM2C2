1e0f6bdd7ffbf6fa39f5437d06dbcb1ba458e007
Nelson Roque <nelsonroquejr@gmail.com>
2019-04-15 15:27:40 -0400

    up

--- R/score_shopping_list.R
+++ R/score_shopping_list.R
@@ -3,7 +3,7 @@
 #' @name score_shopping list
 #' @export
 score_shopping_list <- function(df){
-  scored <- shopping_list %>%
+  scored <- df %>%
     mutate(correct = ifelse(target_price == choice, 1, 0),
            choice_RT = choiceRT)
   return(scored)
