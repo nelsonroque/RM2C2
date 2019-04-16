2693fbac6f59e25827ed905b20c4b55f6a6d9a84
Nelson Roque <nelsonroquejr@gmail.com>
2019-04-16 10:06:34 -0400

    up

--- R/add_metadate_cols.R
+++ R/add_metadate_cols.R
@@ -20,7 +20,13 @@ add_metadate_cols <- function(df, time_var = "timestamp", time_format="%m/%d/%Y"
            TIME_HMS = lubridate::hms(current_datetime),
            TIME_HOUR = lubridate::hour(current_datetime),
            WEEKDAY.value = lubridate::wday(current_datetime),
-           WEEKDAY.label = lubridate::wday(current_datetime,label=T)) %>%
+           WEEKDAY.label = lubridate::wday(current_datetime,label=T),
+           
+           day_hour_round_15 = (lubridate::round_date(current_datetime, "15 minutes")),
+           day_hour_round_30 = (lubridate::round_date(current_datetime, "30 minutes")),
+           
+           hour_round_15 = hms::as.hms(lubridate::round_date(current_datetime, "15 minutes")),
+           hour_round_30 = hms::as.hms(lubridate::round_date(current_datetime, "30 minutes"))) %>%
     mutate(WEEKEND = ifelse(WEEKDAY.label == "Sat" | WEEKDAY.label == "Sun",1,0)) %>%
     mutate(TIME_WINDOW_2hr = cut(TIME_HOUR,
                                  breaks=c(0,8,10,12,14,16,18,20,22,24), 
