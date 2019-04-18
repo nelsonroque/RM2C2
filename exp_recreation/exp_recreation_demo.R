884a53a9976b391a773f444dd3dbcd0fb8f77e4d
Nelson Roque <nelsonroquejr@gmail.com>
2019-04-18 12:49:40 -0400

    up

--- exp_recreation/exp_recreation_demo.R
+++ exp_recreation/exp_recreation_demo.R
@@ -3,7 +3,9 @@ library(readr)
 
 # sample experience recreation dataset
 df <- read_delim("objdata_6.txt", 
-                        "\t", escape_double = FALSE, trim_ws = TRUE)
+                        "\t", escape_double = FALSE, trim_ws = TRUE) %>%
+  mutate(isChangeTrial = ifelse(isChangeTrial == 1, "CHANGE", "ORIGINAL")) %>%
+  mutate(change_trial = factor(isChangeTrial, levels=c("ORIGINAL","CHANGE")))
 
 # get random trial
 single.trial <- df %>% 
@@ -12,10 +14,11 @@ single.trial <- df %>%
 
 # plot trial data
 ggplot(single.trial, aes(x,y,color=obj.color)) + 
-  geom_point(size=9) +
+  geom_point(aes(size=obj_identity)) +
   scale_colour_identity()+
   theme_bw() + 
   theme(panel.background = element_rect(fill = "grey"),
         plot.background = element_rect(fill = "white")) +
-  ggtitle(paste0("Trial #: ", single.trial$trial[1], " | ", single.trial$trial_type[1])) +
-  facet_grid(. ~ isChangeTrial)
+  ggtitle(paste0("Trial #: ", single.trial$trial[1], " | ", 
+                 single.trial$trial_type[1])) +
+  facet_grid(. ~ change_trial)
