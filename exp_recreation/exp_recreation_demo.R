d1f4e012d1a04d67e58939aeb09ba8e6708785d0
Nelson Roque <nelsonroquejr@gmail.com>
2019-04-18 12:38:53 -0400

    up

--- exp_recreation/exp_recreation_demo.R
+++ exp_recreation/exp_recreation_demo.R
@@ -6,11 +6,16 @@ df <- read_delim("objdata_6.txt",
                         "\t", escape_double = FALSE, trim_ws = TRUE)
 
 # get random trial
-single.trial <- df %>% filter(trial == sample(0:max(df$trial),size=1))
+single.trial <- df %>% 
+  filter(trial_type == "change_12") %>%
+  filter(trial == sample(unique(trial),size=1))
 
 # plot trial data
-ggplot(single.trial, aes(x,y,color=obj.color,shape=obj_identity)) + 
+ggplot(single.trial, aes(x,y,color=obj.color)) + 
   geom_point(size=9) +
   scale_colour_identity()+
   theme_bw() + 
-  ggtitle(paste0("Trial #: ", single.trial$trial))
+  theme(panel.background = element_rect(fill = "grey"),
+        plot.background = element_rect(fill = "white")) +
+  ggtitle(paste0("Trial #: ", single.trial$trial[1], " | ", single.trial$trial_type[1])) +
+  facet_grid(. ~ isChangeTrial)
