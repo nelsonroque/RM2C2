063f4abe21c2e45fa020136111ae3e4808704884
Nelson Roque <nelsonroquejr@gmail.com>
2019-04-30 17:01:36 -0400

    up

--- R/score_assoc_fluency.R
+++ R/score_assoc_fluency.R
@@ -3,14 +3,14 @@
 #' @name score_assoc_fluency
 #' @export
 score_assoc_fluency <- function(df) {
-  print("NOTE: Script currently only supports up to 5 entries.")
-  result <- df %>% 
-    mutate(is_entry1 = ifelse(is.na(entry1), 0, 1),
-           is_entry2 = ifelse(is.na(entry2), 0, 1),
-           is_entry3 = ifelse(is.na(entry3), 0, 1),
-           is_entry4 = ifelse(is.na(entry4), 0, 1),
-           is_entry5 = ifelse(is.na(entry5), 0, 1)) %>%
-    rowwise() %>%
-    mutate(total_entries = sum(c(is_entry1, is_entry2, is_entry3, is_entry4, is_entry5)))
-  return(result)
+  max_size <- get_max_col(df, colstring="entry")
+  set_seq <- seq(1,max_size,1)
+  
+  for(i in 1:max_size){
+    varname <- paste0("is_entry", i)
+    new_method <- paste0("ifelse(is.na(entry",i,"), 0, 1)")
+    print(new_method)
+    df <- df %>% mutate_(.dots = setNames(new_method, varname))
+  }
+  return(df)
 }
