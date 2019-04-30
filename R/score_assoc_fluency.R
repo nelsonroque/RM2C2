5e9011664aeaa4c2d929fe518abefce570915e7f
Nelson Roque <nelsonroquejr@gmail.com>
2019-04-30 10:50:15 -0400

    up

--- R/score_assoc_fluency.R
+++ R/score_assoc_fluency.R
@@ -5,11 +5,11 @@
 score_assoc_fluency <- function(df) {
   print("NOTE: Script currently only supports up to 5 entries.")
   result <- df %>% 
-    mutate(is_entry1 = ifelse(entry1 == "NO_DATA", 0, 1),
-           is_entry2 = ifelse(entry2 == "NO_DATA", 0, 1),
-           is_entry3 = ifelse(entry3 == "NO_DATA", 0, 1),
-           is_entry4 = ifelse(entry4 == "NO_DATA", 0, 1),
-           is_entry5 = ifelse(entry5 == "NO_DATA", 0, 1)) %>%
+    mutate(is_entry1 = ifelse(is.na(entry1), 0, 1),
+           is_entry2 = ifelse(is.na(entry2), 0, 1),
+           is_entry3 = ifelse(is.na(entry3), 0, 1),
+           is_entry4 = ifelse(is.na(entry4), 0, 1),
+           is_entry5 = ifelse(is.na(entry5), 0, 1)) %>%
     rowwise() %>%
     mutate(total_entries = sum(c(is_entry1, is_entry2, is_entry3, is_entry4, is_entry5)))
   return(result)
