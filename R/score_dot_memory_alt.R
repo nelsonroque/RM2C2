d18127fbe5627958ae700b7e317c28372d65db88
Nelson Roque <nelsonroquejr@gmail.com>
2019-06-17 18:45:38 -0400

    up

--- R/score_dot_memory_alt.R
+++ R/score_dot_memory_alt.R
@@ -53,18 +53,12 @@ score_dot_memory_alt <- function(df, square_size=5, n_dots=3) {
            r3_perfect = ifelse(r3_min_dist == 0, 1, 0),
            is_perfect_trial = ifelse(r1_min_dist == 0 & r2_min_dist == 0 & r3_min_dist == 0,1,0)) %>%
     rowwise() %>%
-    mutate(hausdorff_distance_forward = pracma::hausdorff_dist(matrix(c(dot1_rx, dot1_ry,
+    mutate(hausdorff_distance = pracma::hausdorff_dist(matrix(c(dot1_rx, dot1_ry,
                                                                         dot2_rx, dot2_ry,
                                                                         dot3_rx, dot3_ry), ncol=2, nrow=3, byrow=T),
                                                                matrix(c(user_dot1_rx, user_dot1_ry,
                                                                         user_dot2_rx, user_dot2_ry,
-                                                                        user_dot3_rx, user_dot3_ry), ncol=2, nrow=3, byrow=T)),
-           hausdorff_distance_backward = pracma::hausdorff_dist(matrix(c(user_dot1_rx, user_dot1_ry,
-                                                                         user_dot2_rx, user_dot2_ry,
-                                                                         user_dot3_rx, user_dot3_ry), ncol=2, nrow=3, byrow=T),
-                                                               matrix(c(dot1_rx, dot1_ry,
-                                                                        dot2_rx, dot2_ry,
-                                                                        dot3_rx, dot3_ry), ncol=2, nrow=3, byrow=T))) %>%
+                                                                        user_dot3_rx, user_dot3_ry), ncol=2, nrow=3, byrow=T))) %>%
     mutate(sum_perfect_dots = sum(c(r1_perfect, r2_perfect, r3_perfect)),
            median_error_distance = median(r1_min_dist, r2_min_dist, r3_min_dist),
            sum_error_distance = sum(r1_min_dist, r2_min_dist, r3_min_dist),
