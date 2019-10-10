36e20fc952b8865cecf25ec33b929a0dd40e94a9
Nelson Roque <nelsonroquejr@gmail.com>
2019-10-10 12:03:54 -0400

    up

--- R/score_shopping_list.R
+++ R/score_shopping_list.R
@@ -2,11 +2,18 @@
 
 #' @name score_shopping list
 #' @export
-score_shopping_list <- function(df){
+score_shopping_list <- function(df, app_version = "1_1_release_18_7_27"){
   PACKAGE.VERSION <- packageVersion("RM2C2")
-  scored <- df %>%
-    mutate(correct = ifelse(target_price == choice, 1, 0),
-           choice_RT = choiceRT) %>%
-    mutate(PACKAGE.VERSION = PACKAGE.VERSION)
+  if(app_version != "1_1_release_18_7_27") {
+    scored <- df %>%
+      mutate(correct = ifelse(target_price == choice, 1, 0),
+             choice_RT = choiceRT) %>%
+      mutate(PACKAGE.VERSION = PACKAGE.VERSION)
+  } else {
+    scored <- df %>%
+      mutate(correct = ifelse(target_price == choice, 1, 0)) %>%
+      mutate(PACKAGE.VERSION = PACKAGE.VERSION)
+  }
+
   return(scored)
 }
