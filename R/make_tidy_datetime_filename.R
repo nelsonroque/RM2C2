e1a01f5bec2991e8d614cc035e6589febe5310d3
Nelson Roque <nelsonroquejr@gmail.com>
2019-12-11 15:52:46 -0500

    up

--- R/make_tidy_datetime_filename.R
+++ R/make_tidy_datetime_filename.R
@@ -12,5 +12,20 @@
 make_tidy_datetime_filename <- function(datetime=Sys.time(), timezone="UTC", prefix="", suffix="") {
   dt <- as.POSIXlt(datetime, timezone, "%Y_%m_%dT%H_%M_%S")
   dt <- paste0(prefix, dt, suffix)
-  return(dt)
+  
+  n <- gsub("%+", "_pct_", dt)
+  n <- gsub("\\$+", "_dollars_", n)
+  n <- gsub("\\++", "_plus_", n)
+  n <- gsub("-+", "_", n)
+  n <- gsub("\\*+", "_star_", n)
+  n <- gsub("#+", "_cnt_", n)
+  n <- gsub("&+", "_and_", n)
+  n <- gsub("@+", "_at_", n)
+  n <- gsub("[^a-zA-Z0-9_]+", "_", n)
+  n <- gsub("([A-Z][a-z])", "_\\1", n)
+  n <- tolower(trimws(n))
+  n <- gsub("(^_+|_+$)", "", n)
+  n <- gsub("_+", "_", n)
+  
+  return(n)
 }
