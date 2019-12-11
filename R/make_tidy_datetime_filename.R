1b31b04ccd6d478b4e05f24c8b6725d54615b725
Nelson Roque <nelsonroquejr@gmail.com>
2019-12-11 15:50:23 -0500

    up

---
+++ R/make_tidy_datetime_filename.R
@@ -0,0 +1,16 @@
+#' RM2C2: Scoring, Summarizing
+
+#' @name make_tidy_datetime_filename
+#' @param datetime class: POSIXct
+#' @param timezone class: string
+#' @param prefix class: string
+#' @param suffix class: string
+#' @import tidyverse
+#' @examples
+#' make_tidy_datetime(datetime=NA, timezone="UTC")
+#' @export
+make_tidy_datetime_filename <- function(datetime=Sys.time(), timezone="UTC", prefix="", suffix="") {
+  dt <- as.POSIXlt(datetime, timezone, "%Y_%m_%dT%H_%M_%S")
+  dt <- paste0(prefix, dt, suffix)
+  return(dt)
+}
