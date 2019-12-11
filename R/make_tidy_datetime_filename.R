0d668fa4ff8266230866d9525322300d1109d2c3
Nelson Roque <nelsonroquejr@gmail.com>
2019-12-11 17:41:11 -0500

    up

--- R/make_tidy_datetime_filename.R
+++ R/make_tidy_datetime_filename.R
@@ -3,11 +3,9 @@
 #' @name make_tidy_datetime_filename
 #' @param datetime class: POSIXct
 #' @param timezone class: string
-#' @param prefix class: string
-#' @param suffix class: string
 #' @import tidyverse
 #' @examples
-#' make_tidy_datetime(datetime=NA, timezone="UTC")
+#' make_tidy_datetime_filename(datetime=NA, timezone="UTC")
 #' @export
 make_tidy_datetime_filename <- function() {
   cur_dts <- Sys.time()
